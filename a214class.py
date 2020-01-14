67fb613fb9047a4bf68d5800758b67dc9ad1c53a
Tyler Hannebique <hannebit@Student-hannebit.local>
2020-01-14 12:05:12 -0600

    digit

--- a214class.py
+++ a214class.py
@@ -1,8 +1,32 @@
 import tkinter as tk
 
+def uppercase(password):
+    for char in password:
+        if char.isupper():
+            return True
+
+def lowercase(password):
+    for char in password:
+        if char.islower():
+            return True
+
+def digit(password):
+    for char in password:
+        if char.isdigit():
+            return True
+
+def sc(password):
+    for char in password:
+        if char in sclist:
+            return True
+
 def login():
-    print("username is :",user_box.get())
-    print("password is :",pass_box.get())
+    password = pass_box.get()
+    if len(password) >= 8 and uppercase(password) and lowercase(password) and digit(password): 
+        print("username is :",user_box.get())
+        print("password is :",pass_box.get())
+    else:
+        print('your password must be at least 8 characters, an upercase and lowercase letter, and a digit')
 
 
 root = tk.Tk()
@@ -18,7 +42,7 @@ user_box.pack()
 pass_label = tk.Label(root, text="password:")
 pass_label.pack()
 
-pass_box = tk.Entry(root)
+pass_box = tk.Entry(root, show='*')
 pass_box.pack()
 
 log_button = tk.Button(root,text = "login", command=login)
